64e12249edab575dd83c4f9ce78ad0e9d39d731b
João Trajano de Souza Neto <47094056+JoaoTrajano@users.noreply.github.com>
2021-01-10 12:36:40 -0300

    Lista de Exercicios de JavaScript

---
+++ quinzena4/lista-de-exercicios/exe3.js
@@ -0,0 +1,49 @@
+// -=-=-=-=-=-= Exercícios de Funções -=-=-=-=-=-= //
+//Exercício 1
+/*
+    var numeros = [];
+
+    for (i = 0; i < 10; i++) {
+    numeros.push(Math.floor(Math.random() * 100));
+    }
+
+    segundoMenorNumero = Infinity;
+    segundoMaiorNumero = 0;
+
+    arrayMaiorNumero = [];
+    arrayMenorNumero = [];
+
+    for(let numero of numeros)
+    {
+        if(numero > segundoMaiorNumero){
+            segundoMaiorNumero = numero;
+            arrayMaiorNumero.push(segundoMaiorNumero);
+        }
+
+        if(numero < segundoMenorNumero){
+            segundoMenorNumero = numero;
+            arrayMenorNumero.push(segundoMenorNumero);
+        }
+    }
+
+    if(arrayMaiorNumero.length == 1){
+        console.log("Segundo maior numero: " + arrayMaiorNumero[0]);
+    }else{
+        console.log("Segundo maior numero: " + arrayMaiorNumero[(arrayMaiorNumero.length - 1) - 1]);
+    }
+
+    if(arrayMenorNumero.length == 1){
+        console.log("Segundo menor numero: " + arrayMenorNumero[0]);
+    }else{
+        console.log("Segundo menor numero: " + arrayMenorNumero[(arrayMenorNumero.length - 1) - 1]);
+    }
+*/
+
+//Exercício 2
+/*
+    funcaoHelloFuture4 = (mensagem) => {
+        alert(mensagem);
+    }
+
+    funcaoHelloFuture4('Hello Future4');
+*/
